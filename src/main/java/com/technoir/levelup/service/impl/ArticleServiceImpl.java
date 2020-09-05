b08ade87714ddb993a7fbbef8bc56d2f832bffa9
Егор Козлов <kozloff130511@gmail.com>
2020-09-05 18:28:15 +0500

    MUFF-9 решил добавить некую сущность для общего доступа, чтобы отображать ее на клиенте без допиливания аутентификации

---
+++ src/main/java/com/technoir/levelup/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,23 @@
+package com.technoir.levelup.service.impl;
+
+import com.technoir.levelup.model.Article;
+import com.technoir.levelup.service.ArticleService;
+
+import java.util.List;
+
+public class ArticleServiceImpl implements ArticleService {
+    @Override
+    public List<Article> getAll() {
+        return null;
+    }
+
+    @Override
+    public Article findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Article> getAllByAuthorUsername(String username) {
+        return null;
+    }
+}
